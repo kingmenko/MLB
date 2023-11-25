a836c80633ae1898fd0359b2b6a1cb8ab9f28282
Amania Enrico <114106522+kingmenko@users.noreply.github.com>
2023-11-25 20:00:36 -0300

    Add files via upload

---
+++ Mambo/index.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_POST['submit']))  {
+    $name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Comment = $_POST['Comment'];
+    $Subject = $_POST['Subject'];
+    
+    
+}
